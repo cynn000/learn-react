de632ffaf2db13e5907c5c8398ddbfcdb8eb4612
Cynthia Nguyen <cynguyen001@gmail.com>
2025-07-30 23:07:46 -0600

    Navbar styling

--- react-facts/src/components/Navbar.jsx
+++ react-facts/src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 
 export default function Navbar() {
      return (
-          <h1>Navbar goes here</h1>
+          <header className="navbar-header">
+               <nav className="navbar-nav">
+                    <img className="navbar-image" src="/src/assets/react-logo.png" alt="React logo" />
+                    <span className="navbar-text">ReactFacts</span>
+               </nav>
+          </header>
      )
 }
