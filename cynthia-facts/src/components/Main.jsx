d061f41628d79fbcff3fa03d690dd2f7ce45f0fd
Cynthia Nguyen <cynguyen001@gmail.com>
2025-07-31 14:08:51 -0600

    Added main content section and styling, added images, updated readme

--- cynthia-facts/src/components/Main.jsx
+++ cynthia-facts/src/components/Main.jsx
@@ -1,6 +1,16 @@
 
 export default function Main() {
      return (
-          <h1>Main here</h1>
+          <main className="main">
+               <h1 className="main-header">
+                    Fun facts about Cynthia
+               </h1>
+               <ul className="facts-list">
+                    <li className="facts-list-item">My favourite video game is Animal Crossing 🎮</li>
+                    <li className="facts-list-item">I like to indoor boulder 💪</li>
+                    <li className="facts-list-item">I am learning Korean 🇰🇷</li>
+                    <li className="facts-list-item">I am a developer that loves to learn 🖥️</li>
+               </ul>
+          </main>
      )
 }
