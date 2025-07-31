566b9caf7a8c81ca91148d635c41319d0f4d252a
Cynthia Nguyen <cynguyen001@gmail.com>
2025-07-31 13:21:27 -0600

    Initial commit for cynthia-facts

---
+++ cynthia-facts/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Main from "./Main"
+import Navbar from "./Navbar"
+
+export default function App() {
+     return (
+          <>
+               <>
+                    <Navbar />
+                    <Main />
+               </>
+          </>
+     )
+}
---
+++ cynthia-facts/src/Main.jsx
@@ -0,0 +1,6 @@
+
+export default function Main() {
+     return (
+          <h1>Main here</h1>
+     )
+}
