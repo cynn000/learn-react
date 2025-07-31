c8ba7b2be803253fcc0d9c71359626515fd5a9e3
Cynthia Nguyen <cynguyen001@gmail.com>
2025-07-31 12:36:47 -0600

    Added main content section and styling

--- react-facts/src/components/Main.jsx
+++ react-facts/src/components/Main.jsx
@@ -1,6 +1,17 @@
 
 export default function Main() {
      return (
-          <h1>Main goes here</h1>
+          <>
+               <main className="main">
+                    <h1 className="main-header">Fun facts about React</h1>
+                    <ul className="facts-list">
+                         <li className="facts-list-item">Was first released in 2013</li>
+                         <li className="facts-list-item">Was originally created by Jordan Walke</li>
+                         <li className="facts-list-item">Has well over 200K stars on GitHub</li>
+                         <li className="facts-list-item">Is maintained by Meta</li>
+                         <li className="facts-list-item">Powers thousands of enterprise apps, including mobile apps</li>
+                    </ul>
+               </main>
+          </>
      )
 }
