b841b8f03be5d7cdd163895d855c5912fb688b7d
Cynthia Nguyen <cynguyen001@gmail.com>
2025-07-31 13:45:49 -0600

    Updated cynthia-facts readme, added navbar and styling

--- cynthia-facts/src/App.jsx
+++ cynthia-facts/src/App.jsx
@@ -1,6 +1,6 @@
 
-import Main from "./Main"
-import Navbar from "./Navbar"
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
 
 export default function App() {
      return (
---
+++ cynthia-facts/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+export default function Navbar() {
+     return (
+          <header className="navbar-header">
+               <nav className="navbar-nav">
+                    <img className="leaf-logo" src="/src/assets/animal-crossing-leaf-logo.png" alt="Animal Crossing Leaf logo" />
+                    <span className="navbar-text">CynthiaFacts</span>
+               </nav>
+          </header>
+     )
+}
