c4c140fbefbcf1928214d8c8babaee2d04b93d30
Cynthia Nguyen <cynguyen001@gmail.com>
2025-07-31 19:57:02 -0600

    Initial commit for travel journal, added app and header component with styling to the header

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+     return (
+          <header className="header">
+               <img className="globe-icon" src="/src/assets/globe.png" alt="Globe icon"/>
+               <h1 className="header-text">my travel journal.</h1>
+          </header>
+     )
+}
