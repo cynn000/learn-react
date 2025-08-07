c7dd072b40a345c35585848a1d011c503849dd87
Cynthia Nguyen <cynguyen001@gmail.com>
2025-08-07 16:18:14 -0600

    Refactor using array state, form submission, and obtaining new ingredient from form

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -1,15 +1,36 @@
 
+import React from "react"
+
 export default function Main() {
+     const [ingredients, setMyIngredients] = React.useState([])
+     const ingredientsListItems = ingredients.map(ingredient => (
+               <li key={ingredient}>
+                    {ingredient}
+               </li>
+          )
+     )
+
+     function handleSubmit(event) {
+          event.preventDefault()
+          const formData = new FormData(event.currentTarget)
+          const newIngredient = formData.get("ingredient")
+          setMyIngredients(prevIngredients => [...prevIngredients, newIngredient])
+     }
+
      return (
           <main className="main">
-               <form className="add-ingredient-form">
+               <form onSubmit={handleSubmit} className="add-ingredient-form">
                     <input className="add-ingredient-input"
                          type="text"
                          placeholder="e.g. oregano"
                          aria-label="Add ingredient"
+                         name="ingredient"
                     />
                     <button className="add-ingredient-button">Add ingredient</button>
                </form>
+               <ul>
+                    {ingredientsListItems}
+               </ul>
           </main>
      )
 }
